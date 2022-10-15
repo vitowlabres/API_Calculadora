fd2e08f39113e91db61de470d680b537242cdc56
vitowlabres <vitorlabres12@gmail.com>
2022-10-15 11:15:16 -0300

    criado arquivo para operacoes e codigos iniciais de testes

---
+++ src/test/operacoes.test.js
@@ -0,0 +1,54 @@
+import test from "node:test"
+import Operacoes from "../controllers/operacoes.js"
+
+// Testes na operação de soma
+test('verifica operação de soma', () => {
+    expect(Operacoes.soma(2,2)).toBe(4)
+})
+
+test('verifica operação de soma', () => {
+    expect(Operacoes.soma('a',2)).toBe('erro')
+})
+
+test('verifica operação de soma', () => {
+    expect(Operacoes.soma(1,'$')).toBe('erro')
+})
+
+// Testes na operação de subtração
+test('verifica operação de subtração', () => {
+    expect(Operacoes.subtracao(5,2)).toBe(3)
+})
+
+test('verifica operação de subtração', () => {
+    expect(Operacoes.subtracao('X',4)).toBe('erro')
+})
+
+test('verifica operação de subtração', () => {
+    expect(Operacoes.subtracao(3,'(')).toBe('erro')
+})
+
+// Testes na operação de multiplicação
+test('verifica operação de multiplicação', () => {
+    expect(Operacoes.multiplicacao(3,5)).toBe(15)
+})
+
+test('verifica operação de multiplicação', () => {
+    expect(Operacoes.multiplicacao(3,'a')).toBe('erro')
+})
+
+test('verifica operação de multiplicação', () => {
+    expect(Operacoes.multiplicacao('@',5)).toBe('erro')
+})
+
+//Testes na operação de divisão
+test('verifica operação de divisão', () => {
+    expect(Operacoes.divisao(4,2)).toBe(2)
+})
+
+test('verifica operação de divisão', () => {
+    expect(Operacoes.divisao('s',2)).toBe('erro')
+})
+
+test('verifica operação de divisão', () => {
+    expect(Operacoes.divisao(4,'-')).toBe('erro')
+})
