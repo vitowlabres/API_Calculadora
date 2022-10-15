c99cd320929d3610c3dd98e8d53e51f952ea4e6a
vitowlabres <vitorlabres12@gmail.com>
2022-10-15 12:23:16 -0300

    criacao da estrutura do controller operacoes

---
+++ src/controllers/operacoes.js
@@ -0,0 +1,19 @@
+export default class Operacoes
+{
+    static adicao = (req,res) => {
+        
+    };
+
+    static subtracao = (req,res) => {
+        
+    };
+
+    static multiplicacao = (req,res) => {
+        
+    };
+
+    static divisao = (req,res) => {
+        
+    };
+
+}
