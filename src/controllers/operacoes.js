5fb8149b9544440dbc9b96e9ae42ee441fa6bead
vitowlabres <vitorlabres12@gmail.com>
2022-10-16 10:25:46 -0300

    operacao de adicao implementada

--- src/controllers/operacoes.js
+++ src/controllers/operacoes.js
@@ -3,11 +3,17 @@ export default class Operacoes
     static adicao = (req,res) => {
         let numero1 = Object.values(req.query)[0]
         let numero2 = Object.values(req.query)[1]
-        res.status(200).json({
-            num1: numero1,
-            num2: numero2,
-            resultado: Number(numero1)+Number(numero2)
-        });
+        if(Number(numero1) == (numero1) && Number(numero2) == (numero2)){
+            res.status(200).json({
+                num1: numero1,
+                num2: numero2,
+                resultado: Number(numero1)+Number(numero2)
+            });    
+        } else {
+            res.status(400).json({
+                mensagem: 'ERRO! Pelo menos um dos valores informados não é um número'
+            });
+        }
     };
 
     static subtracao = (req,res) => {
